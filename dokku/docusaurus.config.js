f02520fd965255583cd4250a0eaa8bbea7750828
André Martins <a89830@ualg.pt>
2025-12-13 17:07:01 +0000

    Personalizei homepag e navbar

--- dokku/docusaurus.config.js
+++ dokku/docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'O meu Site',
+  tagline: 'A melhor loja eletrônica',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -93,7 +93,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Loja',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
