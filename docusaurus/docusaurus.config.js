e6bb67ddfbbfc02d00a5cdb81cf7a6d9f37eab52
a85277 <148544600+a85277@users.noreply.github.com>
2025-12-17 13:11:38 +0000

    Atualizaçao do docussaurus config para conseguir aparecer a homepage

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Loja Dokku',
+  tagline: 'A tua loja de informática',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://lojadokku.pt',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Dokku', // Usually your GitHub org/user name.
+  projectName: 'site_loja_dokku', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
